cccec5746a5dd18a17c515958b9ec81b466ab7f4
Vitor Costa <vitor.jose@mzgroup.com>
2021-01-12 18:46:51 -0300

    add: products route

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const express = require('express')
+const products = require('./products.json')
+
 const PORT = 3000
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+app.get('/products', (req, res) => {
+    res.json({
+        data: products
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('escutando na porta ' + PORT)
+})
